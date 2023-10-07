61479df88a3f57ff2e722280888593a6f8bdbff4
Garret Hampton <djgaspedal@gmail.com>
2023-10-07 17:01:50 -0400

    Update SimulatorRun.java

--- SimulatorRun.java
+++ SimulatorRun.java
@@ -1,16 +1,9 @@
-
-import java.util.*;
-
-import javax.swing.SwingConstants;
-
 public class SimulatorRun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// System.out.println("nate parker");
 		
+		HelloWorldSwing.EventHandlerHelloWorldSwing();
 		
 	}
 
 }
-
