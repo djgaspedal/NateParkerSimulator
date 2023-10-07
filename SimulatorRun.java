34bfdbd93e4a66f3a1b544714009b8a535b2f6d1
Garret Hampton <djgaspedal@gmail.com>
2023-10-07 11:55:32 -0400

    Add files via upload

---
+++ SimulatorRun.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+import javax.swing.SwingConstants;
+
+public class SimulatorRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println("nate parker");
+		
+		
+	}
+
+}
+
